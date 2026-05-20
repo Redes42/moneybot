1a8405c706d501f172a3ee66d3ad4fe138976ffa
Evgeniy Krasnov <krasnoves42@gmail.com>
2026-05-20 15:39:12 +0300

    improve logging, add admin support, ready to test deploy

--- create_admin.py
+++ create_admin.py
@@ -4,3 +4,4 @@ from db.db import SessionLocal
 
 chat_id: int = int(input('Введите id пользователя (чата): '))
 Users.create_user(chat_id=chat_id, is_admin=True)
+print(f'Администратор с chat_id={chat_id} успешно создан!')
